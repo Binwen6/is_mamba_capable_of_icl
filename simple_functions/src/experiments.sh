14f60e40ec1ebc79119c355a34c85e7a3bed117c
Binwen6 <l1045028202@163.com>
2025-04-14 11:59:32 -0700

    Add configuration files for Gaussian Kernel Regression and Nonlinear Dynamical System models; update experiment script for new settings

--- simple_functions/src/experiments.sh
+++ simple_functions/src/experiments.sh
@@ -1,15 +1,33 @@
+# for seed in 1 2 3
+#   do
+#     for setting in skewed_linear_regression linear_regression decision_tree sparse_linear_regression relu_2nn_regression
+#         do
+#           for model in s4 gpt2 mamba
+#             do
+#               # Job to perform
+#               PYTHONPATH=../../. python train.py --config conf/${setting}_${model}.yaml --training.seed ${seed}
+#             done
+#       done
+# done
+
+# # Print some Information about the end-time to STDOUT
+# echo "DONE";
+# echo "Finished at $(date)";
+
+#!/bin/bash
+
 for seed in 1 2 3
+do
+  for setting in linear_regression gaussian_kernel_regression nonlinear_dynamical_system
   do
-    for setting in skewed_linear_regression linear_regression decision_tree sparse_linear_regression relu_2nn_regression
-        do
-          for model in s4 gpt2 mamba
-            do
-              # Job to perform
-              PYTHONPATH=../../. python train.py --config conf/${setting}_${model}.yaml --training.seed ${seed}
-            done
-      done
+    for model in gpt2 mamba
+    do
+      PYTHONPATH=../../. python train.py --config conf/${setting}_${model}.yaml --training.seed ${seed}
+    done
+  done
 done
 
-# Print some Information about the end-time to STDOUT
-echo "DONE";
-echo "Finished at $(date)";
+echo "DONE"
+echo "Finished at $(date)"
+
+# chmod +x run_icl_experiments.sh && ./run_icl_experiments.sh
